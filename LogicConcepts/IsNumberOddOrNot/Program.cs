508002d7313196a5916c9009800d0ccbb40ea4df
Alejandra Aristizabal <alejandra.aristizabal@gco.com.co>
2026-02-15 10:15:37 -0500

    refactor: add number validation and exit option.

--- LogicConcepts/IsNumberOddOrNot/Program.cs
+++ LogicConcepts/IsNumberOddOrNot/Program.cs
@@ -1,16 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Ingrese un número:");
-
-int numero = Convert.ToInt32(Console.ReadLine());
-
-if (numero % 2 == 0)
+﻿do
 {
-    Console.WriteLine("El número es par");
-}
-else
-{
-    Console.WriteLine("El número es impar");
-}
+    Console.Write("Ingrese un número o escriba 'salir' : ");
+    string texto = Console.ReadLine();
 
-Console.ReadLine();
+    if (texto == "salir")
+    {
+        Console.WriteLine("Saliendo del programa...");
+        break;
+    }
+
+    int numero;
+
+    if (int.TryParse(texto, out numero))
+    {
+        
+
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine($"El número {numero}, es par.");
+        }
+        else
+        {
+            Console.WriteLine($"El número {numero}, es impar.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("El dato ingresado no es un número.");
+    }
+
+    Console.WriteLine();
+
+} while (true);
 
