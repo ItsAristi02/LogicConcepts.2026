a77592172e90995793322020dc5f3cad9519d5d5
Alejandra Aristizabal <alejandra.aristizabal@gco.com.co>
2026-02-15 19:54:30 -0500

    feat: add exercise 3, which is a program to enter three numbers, validate numeric and unique values, sort them in ascending order, and display the smallest, middle, and largest.

---
+++ LogicConcepts/SortNumbers/Program.cs
@@ -0,0 +1,48 @@
+﻿using Shared;
+
+
+string opcion = string.Empty;
+List<string> opciones = new List<string> { "si", "no" };
+
+do
+{
+
+    Console.WriteLine("Ingrese 3 números diferentes.");
+
+    int num1 = ConsoleExtension.GetInt("Primer número: ");
+    int num2 = ConsoleExtension.GetInt("Segundo número: ");
+    int num3 = ConsoleExtension.GetInt("Tercer número: ");
+
+    if (num1 == 0 || num2 == 0 || num3 == 0)
+    {
+        Console.WriteLine("Debe ingresar números validos.");
+    }
+    else if (num1 == num2 || num1 == num3 || num2 == num3)
+    {
+        Console.WriteLine("Los números no pueden ser iguales.");
+    }
+    else
+    {
+        int mayor = num1;
+        int menor = num1;
+
+        if (num2 > mayor) mayor = num2;
+        if (num3 > mayor) mayor = num3;
+
+        if (num2 < menor) menor = num2;
+        if (num3 < menor) menor = num3;
+
+        int medio = num1 + num2 + num3 - mayor - menor;
+
+        Console.WriteLine($"El número mayor es {mayor}, el del medio es {medio}, el menor es {menor}");
+    }
+
+    opcion = ConsoleExtension.GetValidOptions("Desea continuar (si/no): ", opciones);
+
+    Console.WriteLine();
+
+
+}
+while (opcion != "no");
+
+Console.WriteLine("¡Gracias por usar el programa!");
