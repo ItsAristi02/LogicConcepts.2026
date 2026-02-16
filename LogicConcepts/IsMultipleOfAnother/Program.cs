fec51dc7a241c0b471884f49c004f76860f8a5ee
Alejandra Aristizabal <alejandra.aristizabal@gco.com.co>
2026-02-15 20:34:19 -0500

    feat: exercise 4 is created, which asks for two numbers and checks whether the second is a multiple of the first.

---
+++ LogicConcepts/IsMultipleOfAnother/Program.cs
@@ -0,0 +1,39 @@
+﻿using Shared;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+do
+{
+    Console.WriteLine("Ingrese dos numeros o escriba 'salir'.");
+
+    string? texto = Console.ReadLine();
+
+    if (texto.Trim().ToLower() == "salir")
+    {
+        Console.WriteLine("Saliendo del programa...");
+        break;
+    }
+
+    int num1 = ConsoleExtension.GetInt("Primer numero: ");
+    int num2 = ConsoleExtension.GetInt("Segundo numero: ");
+
+    if (num1 == 0)
+    {
+        Console.WriteLine("El primer numero no puede ser cero.");
+    }
+    else
+    {
+        if (num2 % num1 == 0)
+        {
+            Console.WriteLine($"El {num2} ES multiplo del primero.");
+        }
+        else
+        {
+            Console.WriteLine($"El {num2} NO es multiplo del primero.");
+        }
+    }
+
+    Console.WriteLine();
+
+}
+while (true);
+
