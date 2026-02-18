9345b629bdc57b784e2c408e4f22677044d1b4b0
Alejandra Aristizabal <alejandra.aristizabal@gco.com.co>
2026-02-17 22:10:35 -0500

    feat: Add exercise 5, which is the validation of leap years with dynamic display of past, present, and future messages.

---
+++ LogicConcepts/IsLeapYear/Program.cs
@@ -0,0 +1,33 @@
+﻿using Shared;
+
+string opcion = string.Empty;
+List<string> opciones = new List<string> { "si", "no" };
+
+do
+{
+    Console.WriteLine("Ingrese un año.");
+    int year = ConsoleExtension.GetInt("Año: ");
+    int currentYear = DateTime.Now.Year;
+
+
+    bool esBisiesto;
+
+    if (year % 4 == 0 && year % 100 != 0)
+        esBisiesto = true;
+    else if (year % 100 == 0 && year % 400 == 0)
+        esBisiesto = true;
+    else
+        esBisiesto = false;
+
+    string complemento = year < currentYear ? "fue" : year == currentYear ? "es" : "será";
+    string resultado = esBisiesto ? "sí" : "no";
+
+    Console.WriteLine($"El año {year} {resultado} {complemento} bisiesto.");
+
+    opcion = ConsoleExtension.GetValidOptions("Desea continuar (si/no): ", opciones);
+
+    Console.WriteLine();
+
+} while (opcion != "no");
+
+Console.WriteLine("¡Gracias por usar el programa!");
